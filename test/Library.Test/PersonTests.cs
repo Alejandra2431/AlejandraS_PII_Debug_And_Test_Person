10a0e118962a280c484beaec585c1e49f5ccf96d
Alejandra2431 <69223047+Alejandra2431@users.noreply.github.com>
2020-09-12 23:40:14 -0300

    Ejercicio hecho

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -13,11 +13,33 @@ namespace Tests
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void nombreCorrecto() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            string nombreCorrecto= "Alejandra"; 
+            Person prueba= new Person(nombreCorrecto,"");
+            Assert.AreEqual(nombreCorrecto,prueba.Name);           
         }
-        
+        [Test]
+        public void cICorrecta()
+        {
+            string cICorrecta= "5.028.441-8"; 
+            Person prueba= new Person("",cICorrecta);
+            Assert.AreEqual(cICorrecta,prueba.ID);           
+        }
+        [Test]
+        public void nombreIncorrecto() // Cambiá el nombre para indicar qué estás probando
+        {
+            string nombreIncorrecto= ""; 
+            Person prueba= new Person(nombreIncorrecto,"");
+            Assert.AreEqual(null,prueba.Name);           
+        }
+        [Test]
+        public void cIIncorrecta()
+        {
+            string cIIncorrecta= "123123123"; 
+            Person prueba= new Person("",cIIncorrecta);
+            Assert.AreEqual(null,prueba.ID);           
+        }
     }
 }
 
